df388060f6112b370e612df01291f7b629ce53f8
Daniel Blake <Daniel Blake>
2017-02-18 12:41:19 +1300

    imporoved mobile responsiveness of swatch container

--- scripts/kakapoColor.js
+++ scripts/kakapoColor.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
   var selectedFill = "fill:#8B4513"
   var clickedSwatch = "";
   var oldSwatch = $('#swatch09');
@@ -12,23 +11,21 @@ $(document).ready(function() {
     setColor(clickedSwatch);
   });
 
-  //When the color class (swatch) is clicked, the function will get the colour code via the data attribute//
+  //When the color class (swatch) is clicked, the function  will get the colour code via the data attribute//
   function setColor(swatch){
     selectedFill = "fill:#"+(swatch.data("color"));
   }
 
-  //Removes the previous swatch highlight, adds the highlight to the clicked swatch.
+  //Removes the previous swatch highlight, adds the highlight to the clicked swatch.//
   function highlightSwatch(){
     oldSwatch.removeClass('selected');
     clickedSwatch.addClass('selected');
     oldSwatch = clickedSwatch;
   }
 
-
-  //When the owl image is clicked the function will check to see which part (id) of the svg image was clicked, and then apply a fill to that part of the image.
+  //When the kakapo image is clicked the function will check to see which part (id) of the svg image was clicked, and then apply a fill to colour that part of the image.
   $("#kakapo-svg").click(function(event){
     $(event.target).attr('style', selectedFill);
   });
-
 
 });
